f4889cbd164b5c0ade3a05a6b663fc8b6f7b7a31
DELL <DELL@DELL-PC>
2014-03-06 12:22:14 +0530

    First Commit

---
+++ src/checkmail/CheckMail.java
@@ -0,0 +1,108 @@
+package checkmail;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Address;
+//import javax.mail.BodyPart;
+import javax.mail.Folder;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.NoSuchProviderException;
+//import javax.mail.Part;
+import javax.mail.Session;
+import javax.mail.Store;
+
+
+
+public class CheckMail {
+    
+    CheckMail(){
+        Properties props=new Properties();
+        Session s=Session.getDefaultInstance(props, null);
+        
+        try{
+            Store store=s.getStore("pop3");
+            String server="pop.asia.secureserver.net";
+            int port=110;
+            String user="priti@airec.co.in";
+            String pass="test123";
+            store.connect(server, port, user, pass);
+            Folder inbox=store.getFolder("Inbox");
+            inbox.open(Folder.READ_ONLY);
+            Message[] messages=inbox.getMessages();
+            
+            for(Message message:messages){
+                for(Address a:message.getFrom()){
+                    System.out.println("From: "+a);
+                    System.out.println("Subject: "+message.getSubject());
+                    //System.out.println("Content: "+message.getContent());
+                    Multipart mp=(Multipart)message.getContent();
+                    System.out.println(mp.toString());
+                    //String cont=getText((Part) message.getContent());
+                    /*for(int i=0;i<mp.getCount();i++) {
+                    BodyPart bodyPart = mp.getBodyPart(i);
+                        //if (bodyPart.isMimeType("text/*")) {
+                        String cont = (String) bodyPart.getContent();
+                        System.out.println("Content: "+cont);
+                        //}
+                    }*/
+                    //System.out.println("Content: "+cont);
+                    System.out.println("-----\n");
+                }
+            }
+            
+        } catch (NoSuchProviderException ex) {
+            Logger.getLogger(CheckMail.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (MessagingException ex) {
+            Logger.getLogger(CheckMail.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(CheckMail.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void main(String args[]){
+        new CheckMail();
+    }
+    /*private boolean textIsHtml = false;
+    private String getText(Part p) throws
+                MessagingException, IOException {
+        if (p.isMimeType("text/*")) {
+            String s = (String)p.getContent();
+            textIsHtml = p.isMimeType("text/html");
+            return s;
+        }
+
+        if (p.isMimeType("multipart/alternative")) {
+            // prefer html text over plain text
+            Multipart mp = (Multipart)p.getContent();
+            String text = null;
+            for (int i = 0; i < mp.getCount(); i++) {
+                Part bp = mp.getBodyPart(i);
+                if (bp.isMimeType("text/plain")) {
+                    if (text == null)
+                        text = getText(bp);
+                    continue;
+                } else if (bp.isMimeType("text/html")) {
+                    String s = getText(bp);
+                    if (s != null)
+                        return s;
+                } else {
+                    return getText(bp);
+                }
+            }
+            return text;
+        } else if (p.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart)p.getContent();
+            for (int i = 0; i < mp.getCount(); i++) {
+                String s = getText(mp.getBodyPart(i));
+                if (s != null)
+                    return s;
+            }
+        }
+
+        return null;
+    }*/    
+}
---
+++ src/sendmail/SendMail.java
@@ -0,0 +1,119 @@
+package sendmail;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class SendMail {
+	
+	SendMail(String host,String user,String pass,String[] to,String subject,String content) throws AddressException, MessagingException{
+		
+            try{
+		//String host="smtp.gmail.com";
+                //String host2="smtp.mail.yahoo.com";
+                host="smtpout.asia.secureserver.net";
+		//String user="binoydalal93@gmail.com";
+                //String user2="testingmyjavamail@yahoo.com";
+                user="priti@airec.co.in";
+		//String pass=".hack%//sign66";
+                //String pass2="Longlongroadon66!";
+                pass="test123";
+		Properties p=System.getProperties();
+                //Properties p2=System.getProperties();
+		p.put("mail.smtp.starttls.enable", "true");
+		p.put("mail.smtp.host", host);
+		p.put("mail.smtp.user", user);
+		p.put("mail.smtp.password", pass);
+		//p.put("mail.smtp.port", "587"); // Gmail
+                //p.put("mail.smtp.port","465");
+                p.put("mail.smtp.port","3535");
+		p.put("mail.smtp.auth", "true");
+                
+                /*p2.put("mail.smtp.starttls.enable", "true");
+		p2.put("mail.smtp.host", host2);
+		p2.put("mail.smtp.user", user2);
+		p2.put("mail.smtp.password", pass2);
+		p2.put("mail.smtp.port", "587"); // Gmail
+                //p.put("mail.smtp.port","465");
+		p2.put("mail.smtp.auth", "true");*/
+
+                //String[] to={"binoydalal93@gmail.com","lttazz99@gmail.com","prerna502@yahoo.co.in","naveenlk.19@gmail.com"};
+		
+		Session s=Session.getDefaultInstance(p, null);
+		/*MimeMessage m=new MimeMessage(s);
+		m.setFrom(new InternetAddress(user));
+				
+                m.setHeader("X-Confirm-Reading-To", user);
+                m.setHeader("Disposition-Notification-To", user);
+                m.setHeader("Return-Receipt-To", user);
+                m.setHeader("X-Errors-To", user);
+                
+		m.setSubject(subject);*/
+                //m.setContent(content,"text/html");		
+                //m.setContent("<html><p><h1>This is my first java mail with an image.</h1></p><p><img src=\"http://www.airecenergyindia.com/test2/Untitled12.jpg\"></p> This mail is a bit different</html>","text/html");
+                //m.setContent("<html><p>This is my first java mail with an image.</p><p><img src=\"http://texasheart.org/HIC/Topics/images/fig20_asd.jpg\"></p></html>","text/html");
+                //m.setText("This is my first java mail from yahoo");
+                
+                /*InternetAddress[] toAdd=new InternetAddress[to.length];
+		
+		for(int i=0;i<to.length;i++)
+			toAdd[i]=new InternetAddress(to[i]);
+		
+		//System.out.println(Message.RecipientType.TO);
+                for (InternetAddress toAdd1 : toAdd) {
+                    m.addRecipient(Message.RecipientType.TO, toAdd1);
+                }*/
+                
+                Transport t=s.getTransport("smtp");
+		t.connect(host, user, pass);
+                
+                for(int i=0;i<to.length;i++){
+                    MimeMessage m=new MimeMessage(s);
+                    m.setFrom(new InternetAddress(user));
+				
+                    m.setHeader("X-Confirm-Reading-To", user);
+                    m.setHeader("Disposition-Notification-To", user);
+                    m.setHeader("Return-Receipt-To", user);
+                    m.setHeader("X-Errors-To", user);
+                
+                    m.setSubject(subject);
+                    content="<html><p><h1>This is my first java mail with an image but there is a difference.</h1></p><p><img src=\"http://www.airecenergyindia.com/ImageLoader2.php?uid="+to[i]+"\"></p></html>";
+                    //content="<html><p><h1>This is my first java mail with an image but there is a difference.</h1></p><p><img src=\"http://www.airecenergyindia.com/ImageLoader2.php\"></p></html>";
+                    m.setHeader("Content-Type", "text/html; charset=us-ascii");
+                    m.setHeader("Content-Transfer-Encoding","7bit");
+                    InternetAddress toAdd=new InternetAddress(to[i]);
+                    m.setRecipient(Message.RecipientType.TO, toAdd);
+                    m.setContent(content,"text/html");
+                    t.sendMessage(m, m.getAllRecipients());
+                    //m.setRecipient(Message.RecipientType.TO, null);
+                }
+		
+                /*Transport t=s.getTransport("smtp");
+                t.connect(host, user, pass);
+                t.sendMessage(m, m.getAllRecipients());*/
+                System.out.println("MESAAGES SENT");
+		t.close();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+                Thread.dumpStack();
+            }
+	}
+	
+        @SuppressWarnings("ResultOfObjectAllocationIgnored")
+        public static void main(String args[]) throws AddressException, MessagingException{
+            String host="smtpout.asia.secureserver.net";
+            String user="priti@airec.co.in";
+            String pass="test123";
+            String[] to={"lttazz99@live.com","testingmyjavamail@yahoo.in","lttazz99@gmail.com","binoydalal93@gmail.com","priti@airec.co.in"};//,"jayc9211@gamil.com"};//,"robindalal@gmail.com","vibhuti1027@gmail.com","prerna502@yahoo.co.in","naveenlk.19@gmail.com","testmyjavamail@yahoo.in"};
+            String subject="Java mail attempt";
+            String content="<html><p><h1>This is my first java mail with an image but there is a difference.</h1></p><p><img src=\"http://www.airecenergyindia.com/ImageLoader2.php\"></p></html>";
+            new SendMail(host,user,pass,to,subject,content);
+        }
+}

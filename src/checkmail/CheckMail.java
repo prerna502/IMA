e4c9579bdac97f2d049c155c822ff5d447027ca7
lttazz99 <lttazz99@gmail.com>
2014-03-07 10:19:21 +0530

    Additions made to checkmail for tracking email id and bounce codes

--- src/checkmail/CheckMail.java
+++ src/checkmail/CheckMail.java
@@ -20,8 +20,11 @@ import javax.mail.Store;
 public class CheckMail {
     
     CheckMail(){
+        System.setProperty("mail.mime.address.strict", "false");
         Properties props=new Properties();
         Session s=Session.getDefaultInstance(props, null);
+        String email="";
+        String code="";
         
         try{
             Store store=s.getStore("pop3");
@@ -35,21 +38,36 @@ public class CheckMail {
             Message[] messages=inbox.getMessages();
             
             for(Message message:messages){
-                for(Address a:message.getFrom()){
-                    System.out.println("From: "+a);
-                    System.out.println("Subject: "+message.getSubject());
-                    //System.out.println("Content: "+message.getContent());
-                    Multipart mp=(Multipart)message.getContent();                                        
-                    for(int i=0;i<mp.getCount();i++) {
-                        BodyPart bodyPart = mp.getBodyPart(i);
-                        if (bodyPart.isMimeType("text/*")) {
-                        String cont = (String) bodyPart.getContent();
-                        System.out.println("Content: "+cont);
-                        }
-                    }
-                    //System.out.println("Content: "+cont);
-                    System.out.println("-----\n");
+                //System.out.println(message.getAllHeaders());
+                /*Address[] address=message.getFrom();
+                String[] add=new String[address.length];
+                for(int i=0;i<address.length;i++){
+                    add[i]=java.net.IDN.toASCII(address[i].toString());
                 }
+                for(String a:add){*/
+                    System.setProperty("mail.mime.address.strict", "false");
+                    //System.out.println("From: "+a);
+                    //System.out.println("Subject: "+message.getSubject()); 
+                    if(message.getSubject().equalsIgnoreCase("Delivery Status Notification")){
+                        //System.out.println("From: "+a);
+                        System.out.println("Subject: "+message.getSubject()); 
+                        Multipart mp=(Multipart)message.getContent();                                        
+                        for(int i=0;i<mp.getCount();i++) {
+                        BodyPart bodyPart = mp.getBodyPart(i);                        
+                            if (bodyPart.isMimeType("text/*")) {
+                            String cont = (String) bodyPart.getContent();
+                            System.out.println("Content: "+cont);
+                            email=cont.substring(cont.indexOf("<")+1, cont.indexOf(">"));
+                            System.out.println("Email: "+email);
+                            code=cont.substring(cont.indexOf("::")+3, cont.indexOf("<")-1);
+                            System.out.println("Code: "+code);
+                            }
+                        }
+                        System.out.println("-----\n");
+                    }
+                    //System.out.println("Content: "+message.getContent());                    
+                    //System.out.println("Content: "+cont);
+                    //System.out.println("-----\n");                
             }
             
             inbox.close(true);
